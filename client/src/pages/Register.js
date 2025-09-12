ef54141534b63f42bfc9c2a9a77fc3acaffe6906
shubham <shubhamborade@gmail.com>
2025-09-12 17:22:01 +0530

    chnges in url

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -11,7 +11,7 @@ const Register = () => {
   const handleRegister = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post(`${API_URL}/api/register`, { email, password });
+      const res = await axios.post(`${API_URL}/register`, { email, password });
       console.log("✅ Registered:", res.data);
       alert('Registered successfully!');
     } catch (err) {
